50f6b1b2d08d25122abe77e4e12e3ccf473e4e21
ANIRUDH <anirudhsudheer@gmail.com>
2022-07-08 06:54:46 +0530

    eVA

---
+++ plugins/menu.js
@@ -0,0 +1,35 @@
+exports.run = {
+   usage: ['menu', 'help', 'bot'],
+   async: async (m, {
+      client,
+      isPrefix
+   }) => {
+      let rows = [{
+         title: 'DOWNLOADER',
+         rowId: `${isPrefix}menutype 1`,
+         description: ``
+      }, {
+         title: 'GROUP TOOLS',
+         rowId: `${isPrefix}menutype 2`,
+         description: ``
+      }, {
+         title: 'UTILITIES',
+         rowId: `${isPrefix}menutype 3`,
+         description: ``
+      }, {
+         title: 'OWNER TOOLS',
+         rowId: `${isPrefix}menutype 4`,
+         description: ``
+      }]
+      let text = 'An autonomous program on the internet or another network that can interact with systems or users.\n\n'
+      text += '◦ *Database* : PostgreSQL\n'
+      text += '◦ *Library* : Baileys v4.3.0\n'
+      text += '◦ *Rest API* : https://api.nxr.my.id\n'
+      text += '◦ *Source* : https://github.com/neoxr/neoxr-bot *(v2.2.0)*\n\n'
+      text += 'If you find an error or want to upgrade premium plan contact the owner.'
+      await client.sendList(m.chat, '', text, '', 'Tap!', [{
+         rows
+      }], m)
+   },
+   error: false
+}
